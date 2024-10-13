3c009f667074d339564a56441cffc9dd0371c4da
JocelynVelarde <a01285780@itesm.mx>
2024-10-12 22:55:45 -0400

    refactor: Update prompt in video_analyzer.py

--- models/video_analyzer.py
+++ models/video_analyzer.py
@@ -103,7 +103,8 @@ def openai_analsis(video_name : str, video_ext : str = 'mp4', objectContext: str
                 "content": [
                     {
                         "type": "text",
-                        "text": f"Based off the provided video and information from different part of the system, can you tell if there are any suspicious or dangerous activities happening in regards to shoplifting? {objectContext} is the context of the objects in the video and {facialContext} is the context of the faces in the video. Please include this information in your analysis if any, and emphasize that these are predicted people/objects. If there exists any of the contexts then just skip over and dont address it."  
+                        "text": f"""Based off the provided video and information from different part of the system, can you tell if there are any suspicious or dangerous activities happening in regards to shoplifting? 
+                                {objectContext} is the context of the objects in the video and {facialContext} is the context of the faces in the video. Please include this information in your analysis if any, and emphasize that these are predicted people/objects. If there exists any of the contexts then just skip over and dont address it."""  
 
                     }
                 ] + encoded_images
